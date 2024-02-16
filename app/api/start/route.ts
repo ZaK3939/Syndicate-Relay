5dda86ead4a72765c3c8f2b814cdd04f93af0d39
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-16 23:17:33 +0900

    feat; update

--- app/api/start/route.ts
+++ app/api/start/route.ts
@@ -18,14 +18,13 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   console.log('isValid', isValid, 'message', message);
   if (message?.button === 1 && isValid && allowedOrigin(message)) {
     const isActive = message.raw.action.interactor.active_status === 'active';
-    const query = `{
-      "query": "query philandList { 
+    const query = `
+      query { 
       philandList
         (input: 
           {address: "0x5037e7747fAa78fc0ECF8DFC526DcD19f73076ce", transparent: false}) 
             { data { name landurl imageurl } 
           } 
-        "
     }`;
     const result = await retryableApiPost<LandResponse>(PHI_GRAPH, query);
 
