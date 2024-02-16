28620b8a26011394374f815f46f782670127534b
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-16 11:58:53 +0900

    test: update

--- app/page.tsx
+++ app/page.tsx
@@ -2,43 +2,71 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 import { NEXT_PUBLIC_URL } from './config';
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: 'Story time!',
-    },
-    {
-      action: 'link',
-      label: 'Link to Google',
-      target: 'https://www.google.com',
-    },
-    {
-      label: 'Redirect to pictures',
-      action: 'post_redirect',
-    },
-  ],
-  image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
-    aspectRatio: '1:1',
-  },
-  input: {
-    text: 'Tell me a boat story',
-  },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-});
+export async function generateMetadata(): Promise<Metadata> {
+  // const { name } = await getCollection();
+  const name = 'zak3939';
+  const frameMetadata = getFrameMetadata({
+    buttons: [
+      {
+        label: 'Check eligibility',
+      },
+    ],
+    image: `${NEXT_PUBLIC_URL}/api/images/start`,
+    post_url: `${NEXT_PUBLIC_URL}/api/start`,
+  });
 
-export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
-  description: 'LFG',
-  openGraph: {
-    title: 'zizzamia.xyz',
-    description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
+  return {
+    title: name,
+    description: "Check if you're eligible for a free mint",
+    openGraph: {
+      title: name,
+      description: "Check if you're eligible for a free mint",
+      images: [`${NEXT_PUBLIC_URL}/api/images/start`],
+    },
+    other: {
+      ...frameMetadata,
+      'fc:frame:image:aspect_ratio': '1:1',
+    },
+  };
+}
+
+// const frameMetadata = getFrameMetadata({
+//   buttons: [
+//     {
+//       label: 'Story time!',
+//     },
+//     {
+//       action: 'link',
+//       label: 'Link to Google',
+//       target: 'https://www.google.com',
+//     },
+//     {
+//       label: 'Redirect to pictures',
+//       action: 'post_redirect',
+//     },
+//   ],
+//   image: {
+//     src: `${NEXT_PUBLIC_URL}/park-3.png`,
+//     aspectRatio: '1:1',
+//   },
+//   input: {
+//     text: 'Tell me a boat story',
+//   },
+//   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+// });
+
+// export const metadata: Metadata = {
+//   title: 'zizzamia.xyz',
+//   description: 'LFG',
+//   openGraph: {
+//     title: 'zizzamia.xyz',
+//     description: 'LFG',
+//     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+//   },
+//   other: {
+//     ...frameMetadata,
+//   },
+// };
 
 export default function Page() {
   return (
