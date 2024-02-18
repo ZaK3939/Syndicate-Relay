51a8f594894de18238357ea88093eee27e12b1f5
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-18 17:19:23 +0900

    feat: update

--- app/api/relay/route.ts
+++ app/api/relay/route.ts
@@ -1,14 +1,14 @@
-import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit';
-import { kv } from '@vercel/kv';
-import { NextRequest, NextResponse } from 'next/server';
-import { NEXT_PUBLIC_URL, PHI_GRAPH, queryForLand } from '../../config';
-import { getAddressButtons } from '../../lib/addresses';
-import signMintData from '../../lib/signMint';
-import { allowedOrigin } from '../../lib/origin';
-import { getFrameHtml } from '../../lib/getFrameHtml';
-import { errorResponse, mintResponse } from '../../lib/responses';
-import { LandResponse, Session } from '../../lib/types';
-import { retryableApiPost } from '../../lib/retry';
+import { FrameRequest, getFrameMessage } from "@coinbase/onchainkit";
+import { kv } from "@vercel/kv";
+import { NextRequest, NextResponse } from "next/server";
+import { NEXT_PUBLIC_URL, PHI_GRAPH, queryForLand } from "../../config";
+import { getAddressButtons } from "../../lib/addresses";
+import signMintData from "../../lib/signMint";
+import { allowedOrigin } from "../../lib/origin";
+import { getFrameHtml } from "../../lib/getFrameHtml";
+import { errorResponse, mintResponse } from "../../lib/responses";
+import { LandResponse, Session } from "../../lib/types";
+import { retryableApiPost } from "../../lib/retry";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
@@ -28,43 +28,49 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       );
     }
 
-    const isActive = message.raw.action.interactor.active_status === 'active';
+    const isActive = message.raw.action.interactor.active_status === "active";
     const fid = message.interactor.fid;
     let session = ((await kv.get(`session:${fid}`)) ?? {}) as Session;
-    console.log('message.interactor', message.interactor);
+    console.log("message.interactor", message.interactor);
     const address = message.interactor.verified_accounts[0].toLowerCase();
     const result = await retryableApiPost<LandResponse>(PHI_GRAPH, {
       query: queryForLand(address),
     });
 
-    if ((isActive || (result.data && result.data.philandList.data)) && session?.address) {
+    if (
+      (isActive || (result.data && result.data.philandList.data)) &&
+      session?.address
+    ) {
       const { address } = session;
       const sig = await signMintData({
         to: address,
         tokenId: 1,
         fid,
       });
-      console.log('body.trustedData', body.trustedData);
-      let functionSignature = 'mint(address to)';
+      console.log("body.trustedData", body.trustedData);
+      let functionSignature = "mint(address to)";
       const postBody = JSON.stringify({
         frameTrustedData: body.trustedData.messageBytes,
         // contractAddress: process.env.MINER_CONTRACT_ADDRESS,
-        contractAddress: '0x3221679c531bcf7eb4f728bbad3f4301d2e2d640',
+        contractAddress: "0x3221679c531bcf7eb4f728bbad3f4301d2e2d640",
         functionSignature: functionSignature,
         args: { to: address },
         // functionSignature: 'mint(address to, uint256 tokenId, uint256 fid, bytes calldata sig)',
         // args: { to: '{frame-user}', amount: 1, fid: fid, sig: sig },
       });
-      console.log('postBody', postBody);
-      const res = await fetch('https://frame.syndicate.io/api/v2/sendTransaction', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.SYNDICATE_API_KEY}`,
+      console.log("postBody", postBody);
+      const res = await fetch(
+        "https://frame.syndicate.io/api/v2/sendTransaction",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.SYNDICATE_API_KEY}`,
+          },
+          body: postBody,
         },
-        body: postBody,
-      });
-      console.log('response syndicate frame', res);
+      );
+      console.log("response syndicate frame", res);
 
       if (res.status === 200) {
         const {
@@ -78,23 +84,23 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
             `https://frame.syndicate.io/api/transaction/${transactionId}/hash`,
             {
               headers: {
-                'content-type': 'application/json',
+                "content-type": "application/json",
                 Authorization: `Bearer ${process.env.SYNDICATE_API_KEY}`,
               },
             },
           );
           if (res.status === 200) {
             console.log(
-              'res',
+              "res",
               res,
               `https://frame.syndicate.io/api/transaction/${transactionId}/hash`,
-              'go to check',
+              "go to check",
             );
             return new NextResponse(
               getFrameHtml({
                 buttons: [
                   {
-                    label: '🔄 Check status',
+                    label: "🔄 Check status",
                   },
                 ],
                 post_url: `${NEXT_PUBLIC_URL}/api/check`,
@@ -108,11 +114,11 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     } else {
       return mintResponse();
     }
-  } else return new NextResponse('Unauthorized', { status: 401 });
+  } else return new NextResponse("Unauthorized", { status: 401 });
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
   return getResponse(req);
 }
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
