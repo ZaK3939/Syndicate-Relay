9e8be4d7f7f1e00284aeb64477e0c8b0d42d0896
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-27 00:09:18 +0900

    feat: update

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,4 @@
-export const NEXT_PUBLIC_URL =
-  "https://a-frame-in-100-lines-seven-khaki.vercel.app";
+export const NEXT_PUBLIC_URL = "https://demo-non-field-rpg.vercel.app";
 
 export const ALLOWED_ORIGIN = "a-frame-in-100-lines-seven-khaki.vercel.app";
 
