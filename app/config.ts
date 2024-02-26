0595b63111e0f954fff2add1b402745ffb131c95
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-26 23:56:20 +0900

    feat:update config

--- app/config.ts
+++ app/config.ts
@@ -3,7 +3,7 @@ export const NEXT_PUBLIC_URL =
 
 export const ALLOWED_ORIGIN = "a-frame-in-100-lines-seven-khaki.vercel.app";
 
-export const NFT_ADDRESS = "0xdd902764addcff1c1874f2675a123352f1a8e7fd";
+export const NFT_ADDRESS = "0xf74661d961f16807ebb96aeff60c63afcd5a005f";
 
 export const CARD_DIMENSIONS = {
   width: 800,
