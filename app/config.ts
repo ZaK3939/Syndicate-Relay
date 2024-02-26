e95a78bfa3c59769bd1a650e14cf47baa0c71a8f
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-27 00:11:41 +0900

    feat: update

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
-export const NEXT_PUBLIC_URL = "https://demo-non-field-rpg.vercel.app";
+export const NEXT_PUBLIC_URL = "https://syndicate-nft-recast.vercel.app/";
 
-export const ALLOWED_ORIGIN = "a-frame-in-100-lines-seven-khaki.vercel.app";
+export const ALLOWED_ORIGIN = "syndicate-nft-recast.vercel.app";
 
 export const NFT_ADDRESS = "0xf74661d961f16807ebb96aeff60c63afcd5a005f";
 
@@ -9,4 +9,4 @@ export const CARD_DIMENSIONS = {
   height: 800,
 };
 
-export const TOKEN_IMAGE = `${NEXT_PUBLIC_URL}/horse.png`;
+export const TOKEN_IMAGE = `${NEXT_PUBLIC_URL}/shark.png`;
